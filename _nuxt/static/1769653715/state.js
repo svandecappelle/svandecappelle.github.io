8028946230653b5c3e2b7a8cbb9aa187ce08e480
Steeve Vandecappelle <steeve.vandecappelle@corp.ovh.com>
2026-01-29 03:45:45 +0100

    public new version

---
+++ _nuxt/static/1769653715/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1769653715",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"064b8f9f"}}}}(null,"\u002F"));
